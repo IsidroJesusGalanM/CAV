15d3ca89f8c8674768de95c50f423f9a8af6e98d
juxx <isidrojesus117@gmail.com>
2023-05-31 14:35:22 -0600

    update views

---
+++ app/src/main/java/com/example/cav/details_museum_activity.kt
@@ -0,0 +1,11 @@
+package com.example.cav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class details_museum_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_museum)
+    }
+}
